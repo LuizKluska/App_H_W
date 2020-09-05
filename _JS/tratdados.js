11a3613c4298f4851bf918b50abe9ab06ba7603e
Kluska L <69592113+LuizKluska@users.noreply.github.com>
2020-09-04 21:33:53 -0300

    04/09/2020
    04/09/2020

--- _JS/tratdados.js
+++ _JS/tratdados.js
@@ -1,18 +1,17 @@
-
-function carrega_dados(n){
+/* function envia_dados(){
     
-    console.log(`Arquivo carregado: ${n}`)
-    
+   
+    var caminho_do_arquivo = document.getElementById('caminho_do_arquivo').value;
+    console.log(caminho_do_arquivo)
+    res.innerHTML = caminho_do_arquivo;
     const fs = require('fs');
-    var arquivo = fs.readFileSync(n,'utf-8');
+    var arquivo = fs.readFileSync(caminho_do_arquivo,'utf-8');
     var lista_grande = arquivo.split('\r\n');
+    var res = document.getElementById('res');
+    
+    
+    
+    
 
-    for (v of lista_grande) {
-        console.log(v);
     }
-
-}
-carrega_dados('AN.txt')
-
-
-
+*/
